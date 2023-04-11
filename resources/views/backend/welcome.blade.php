a4daadf2519f74556692f6807ba61722b64ea15b
unknown <dadanhidyt@gmail.com>
2023-04-11 21:21:23 +0700

    dashboard

--- resources/views/backend/welcome.blade.php
+++ resources/views/backend/welcome.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.backend')
 @section('main')
-<div class="row">
-    <div class="alert-section col-12">
-        <p class="alert alert-warning">Silahkan update akun anda</p>
-    </div>
-    
-    <main class="col-12">
-        <!-- sekilas toko -->
+    <div class="row">
+        <div class="alert-section col-12">
+            <p class="alert alert-warning">Silahkan update akun anda</p>
+        </div>
 
-        <!-- sek -->
-    </main>
-</div>
+        <main class="col-12">
+            <!-- sekilas toko -->
+            <div class="card card-custom">
+                <div class="card-header bg-primary">
+                    <div class="card-title text-white">Penjualan</div>
+                    <div id="filter" class="card-toolbar">
+                        <form action="">
+                            <select class="form-control" name="" id="">
+                                <option value="">Hari</option>
+                                <option value="">Bulan</option>
+                            </select>
+                        </form>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <!-- rafly antum ngerjaken -->
+                    </div>
+                </div>
+            </div>
+            <!-- sek -->
+        </main>
+    </div>
 @endsection
