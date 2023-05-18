bd0f18f9f7381bded0bd86127c38daa761f51eee
raflyfrmnsyh <raflyfirmansyah017@gmail.com>
2023-05-18 14:52:31 +0700

    menambah beberapa dihalaman laporan penjualan kasir

--- resources/views/kasir/laporan-penjualan.blade.php
+++ resources/views/kasir/laporan-penjualan.blade.php
@@ -1,124 +1,172 @@
 <x-kasir-layout>
-    <div class="container">
-        <div class="card mt-3">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-9 col-sm-12">
-                        <h3 class="fw-bolder">Laporan Penjualan</h3>
-                    </div>
-                    <div class="col-md-3 col-sm-12">
-                        <a href="#" class="btn btn-info d-flex align-items-center justify-content-center text-white">
-                            <i class="bx bx-cloud-download fs-4 mx-2"></i>
-                            <span>Download Laporan</span>
-                        </a>
+
+    <div class="container-md">
+        <div class="row mt-4">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-header d-flex align-items-center justify-content-between border-1 border-bottom pb-4 pt-2">
+                            <h3 class="fw-bolder">Laporan Penjualan</h3>
+                            <div class="d-flex align-items-center">
+                                <button class="btn btn-primary d-flex align-items-center text-white pe-3 me-3">
+                                    <i class="bx bx-cloud-download me-2"></i>
+                                    <span>Download</span>
+                                </button>
+                                <button type="button" class="btn btn-info text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    Tambah Pengeluaran
+                                  </button>
+                            </div>
+                        </div>
+
+                        <div class="row mt-4">
+                            <div class="col-md-3">
+                                <div class="dash-widget">
+                                    <div class="dash-widgetimg">
+                                        <span><img src="../assets/img/icons/dash1.svg" alt="img"></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" id="{{ $kas }}">{{ formatRupiah($kas) }}</span></h5>
+                                        <h6>Dana Kas</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget dash1">
+                                    <div class="dash-widgetimg">
+                                        <span><img src="../assets/img/icons/dash2.svg" alt="img"></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" id="{{ $total_transaksi }}">{{ formatRupiah($total_transaksi) }}</span></h5>
+                                        <h6>Total Transaksi</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget dash2">
+                                    <div class="dash-widgetimg">
+                                        <span><img src="../assets/img/icons/dash3.svg" alt="img"></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" id="{{ $penghasilan }}">{{ formatRupiah($penghasilan) }}</span></h5>
+                                        <h6>Uang Masuk</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget dash3">
+                                    <div class="dash-widgetimg">
+                                        <span><img src="../assets/img/icons/dash4.svg" alt="img"></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" >0</span></h5>
+                                        <h6>Uang Keluar</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget dash1">
+                                    <div class="dash-widgetimg">
+                                        <span><img src="../assets/img/icons/dash2.svg" alt="img"></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" >{{ formatRupiah($penghasilan_bersih) }}</span></h5>
+                                        <h6>Pembayaran Cash</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget dash2">
+                                    <div class="dash-widgetimg">
+                                        <span><i class="bx bxs-credit-card fs-4 text-info"></i></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters" >{{ formatRupiah($penghasilan_bersih) }}</span></h5>
+                                        <h6>Pembayaran Debit</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="dash-widget">
+                                    <div class="dash-widgetimg">
+                                        <span><i class="bx bxs-wallet-alt fs-5 text-warning"></i></span>
+                                    </div>
+                                    <div class="dash-widgetcontent">
+                                        <h5>Rp.<span class="counters">{{ formatRupiah($penghasilan_bersih) }}</span></h5>
+                                        <h6>Dana Kas</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash1.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>Rp.<span class="counters" id="{{ $kas }}">{{ formatRupiah($kas) }}</span></h5>
-                        <h6>Dana Kas</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash1">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash2.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5><span class="counters" id="{{ $total_transaksi }}">{{ formatRupiah($total_transaksi) }}</span></h5>
-                        <h6>Total Transaksi</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash2">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash3.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>Rp.<span class="counters" id="{{ $penghasilan }}">{{ formatRupiah($penghasilan) }}</span></h5>
-                        <h6>Uang Masuk</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash3">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash4.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>Rp.<span class="counters" >{{ formatRupiah($penghasilan_bersih) }}</span></h5>
-                        <h6>Cash</h6>
-                    </div>
-                </div>
-            </div>
-             <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash3">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash4.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>Rp.<span class="counters" >{{ formatRupiah($penghasilan_bersih) }}</span></h5>
-                        <h6>E-Money</h6>
-                    </div>
-                </div>
-            </div>
-             <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash3">
-                    <div class="dash-widgetimg">
-                        <span><img src="../assets/img/icons/dash4.svg" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>Rp.<span class="counters" >{{ formatRupiah($penghasilan_bersih) }}</span></h5>
-                        <h6>Transfer</h6>
-                    </div>
-                </div>
-            </div>
-            
-            {{-- <div class="col-lg-4 col-sm-6 col-12 d-flex">
-                <div class="dash-count das3">
-                    <div class="dash-counts">
-                        <h4>z</h4>
-                        <h5>Penjualan Harian</h5>
-                    </div>
-                    <div class="dash-imgs">
-                        <i data-feather="file"></i>
-                    </div>
-                </div>
-            </div> --}}
         </div>
 
-        <div class="row">
-            <div class="col-lg-4 col-sm-6 col-12 d-flex">
-                <div class="dash-count">
-                    <div class="dash-counts">
-                        <h4>{{ $pelanggan }}</h4>
-                        <h5>Pelanggan</h5>
-                    </div>
-                    <div class="dash-imgs">
-                        <i data-feather="user"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 col-12 d-flex">
-                <div class="dash-count das2">
-                    <div class="dash-counts">
-                        <h4>{{ $belum_bayar }}</h4>
-                        <h5>Pesanan Belum Bayar</h5>
-                    </div>
-                    <div class="dash-imgs">
-                        <i data-feather="file-text"></i>
+        <div class="row mt-1">
+            <div class="col-lg-12">
+                <div class="card py-2">
+                    <div class="card-body">
+                        <table class="table  datanew py-4">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama Pengeluaran</th>
+                                    <th>Jumlah</th>
+                                    <th>Keterangan</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Marsudi Rajasa</td>
+                                    <td>0479 9554 033</td>
+                                    <td>323357259</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Pengeluaran</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bx bx-x text-danger text-hover-white"></i>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form action="#">
+                    <div class="form-group">
+                        <div class="mb-3">
+                            <label for="namaPengeluaran" class="form-label">Nama Pengeluaran <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="namaPengeluaran" aria-describedby="textHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="jumlahPengeluaran" class="form-label">Jumlah Pengeluaran <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="jumlahPengeluaran" aria-describedby="textHelp">
+                        </div>
+                        <div class="mb-4">
+                            <label for="jumlahPengeluaran" class="form-label">Keterangan <span class="text-danger">*</span></label>
+                            <textarea class="form-control" placeholder="Tulis Keterangan" id="floatingTextarea"></textarea>
+                        </div>
+                        <div><button type="submit" class="btn btn-primary w-100">Simpan</button></div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
     </div>
+
 </x-kasir-layout>
